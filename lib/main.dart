1ea8a1f87f56facd212340062e9e3dc2d444c32a
mhmdjavadcs <mhmdjavad.safi@ut.ac.ir>
2024-07-21 17:50:04 +0330

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,70 +7,45 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hiiiiiiiiiiiiiiiiii',
-      theme: ThemeData(
-
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'yoooohooooooo'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const RootPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RootPage> createState() => _RootPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('My Fist App using Flutter!',
+            style: TextStyle(
+              color: Colors.white70,
+            )),
+        backgroundColor: Colors.green,
       ),
-
-      body: Center(
-
-        child: Column(
-      
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('Hello, world!'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => {},
+        backgroundColor: Colors.green[400],
+        child: const Icon(
+          Icons.add,
+          color: Colors.white70,
+        ),
+      ),
     );
   }
 }
